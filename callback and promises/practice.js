349c641562d0fa65e363537941a5223141605fd2
omaimaqasim <omaimaakhan15@gmail.com>
2026-03-13 06:21:15 +0500

    practice callback and promise

--- callback and promises/practice.js
+++ callback and promises/practice.js
@@ -14,5 +14,23 @@ get_user(22,"zein",function (user) {
 console.log("get user with promise\n")
 
 function get_p_user(id,name) {
-    
+        let user = {id:id,name:name};
+        let myuser_promise = new Promise((resolve, reject) => {
+            if (user.id==22 && user.name =="zein") {
+            //   remember this resolve only take one argument
+                resolve(user)
+            }
+            else{
+                reject("enter correct user id and name");
+            }
+        })
+
+        myuser_promise
+        .then((u)=>{
+            console.log(u.id,u.name)
+        }).catch((e)=>{
+             console.log(e)
+        })
 }
+
+get_p_user(22,"zein");
